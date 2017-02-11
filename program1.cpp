7dfd9aac620281de9e19467d8e4d33d6011daae1
evinda <evinda2404@gmail.com>
2017-02-11 13:47:01 +0700

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;// program kalkulator
+// I.S
+// F.S
+
+int main() {
+	// Kamus
+	int a, b;
+	int c;
+	float f;
+	// algoritma
+
+	a = 10;
+	b = 20;
+	c = a + b;
+
+	cout << c << endl ;
+	f = 70.0/3.0;
+	cout << f << endl ;
+	return 0;
+}
+ 
+ 
