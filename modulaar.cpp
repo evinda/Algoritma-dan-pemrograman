155d93fd64ba0a45fc5a851a7be6bfa6b30bbc70
evinda <evinda2404@gmail.com>
2017-03-26 12:27:03 +0700

    Add files via upload

---
+++ modulaar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int a,b;
+void kali () {
+	 int p;
+	 p = a* b ;
+	cout << "hasil kali = " << p << endl;
+	 }
+int main(){
+	
+	
+		cout <<"masukan nilai a" << endl ;
+		cin >> a;
+		cout <<"masukan nilai b" << endl ;
+		cin >> b;
+	
+kali();
+
+	return 0;
+}
